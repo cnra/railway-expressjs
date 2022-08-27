3c37573fbfb2b5c130ca9a0f5075a017ae23fd07
cnra <roser137@hotmail.com>
2022-08-27 13:58:50 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ Hello: "World 3" });
+  res.json({ Hello: "World 4" });
 });
 
 app.listen(port, () => {
